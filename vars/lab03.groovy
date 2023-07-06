442b79173481eb2b18ac7eab95d589d83d3bc3b6
Ali-ELHaggar <93692947+Ali-ELHaggar@users.noreply.github.com>
2023-07-06 13:13:23 +0300

    Update lab03.groovy

--- vars/lab03.groovy
+++ vars/lab03.groovy
@@ -1,1 +1,45 @@
+def call() {
+    pipeline {
+        agent any
+        stages {
+            prepareStage()
+            checkStage()
+            testStage()
+            deploymentStage()
+        }
+    }
+}
+
+def prepareStage() {
+    stage("prepare") {
+        steps {
+            echo "preparing your code"
+        }
+    }
+}
+
+def checkStage() {
+    stage("check") {
+        steps {
+            echo "checking your code"
+        }
+    }
+}
+
+def testStage() {
+    stage("test") {
+        steps {
+            echo "testing your code"
+        }
+    }
+}
+
+def deploymentStage() {
+    stage("deployment") {
+        steps {
+            echo "your code is deployed right now"
+            echo "this build number $BUILD_NUMBER"
+        }
+    }
+}
 
